0815d9260cf5911cf903b39697da9143e297e40a
Nelson Choque <nelsonchoque03@gmail.com>
2023-01-29 22:57:56 -0500

    añadiendo data de los productos

--- js/index.js
+++ js/index.js
@@ -1,7 +1,16 @@
 
+
+import dataProductos from './../data/productos.json' assert {type: 'json'};
+
 const $buttonClose = document.getElementById("button-close");
 const $menuMobile = document.getElementById("menu-mobile");
 const $iconMenuMobile = document.getElementById("icon-menu-mobile");
+const $products = document.getElementById("products");
+
+const $templateProducto = document.getElementById("fragment-producto").content;
+const $fragmento = document.createDocumentFragment();
+
+
 
 
 $buttonClose.addEventListener("click", ()=>{
@@ -23,4 +32,44 @@ $menuMobile.addEventListener("click",()=>{
 
 })
 
-console.log($buttonClose)
+
+const main = ()=>{
+
+    const {productos} = dataProductos; 
+
+    productos.forEach( producto => {
+
+        let $nombre = $templateProducto.querySelector(".products__item__title")
+
+        $nombre.textContent= producto.nombre;
+
+        let $imagen = $templateProducto.querySelector(".products__item__image__img")
+
+        $imagen.setAttribute("src",producto.imagen)
+
+        let $caracteristicas = $templateProducto.querySelectorAll(".characteristics__value")
+
+        $caracteristicas.forEach( (caracteristica,index) =>{
+
+            caracteristica.textContent = producto.caracteristicas[index].descripcion;
+            
+        })
+
+
+        let $clone = document.importNode($templateProducto,true)
+
+        $fragmento.append($clone)
+    })
+
+
+    $products.appendChild($fragmento)
+
+}
+
+
+main();
+
+
+
+
+
